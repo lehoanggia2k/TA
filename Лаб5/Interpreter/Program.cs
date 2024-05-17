b23bf9185cc4d336c333811bd1314bc92bee1ba4
lehoanggia2k <66256362+lehoanggia2k@users.noreply.github.com>
2024-05-17 22:48:36 +0300

    commit17/5
    17/5/2024

---
+++ Лаб5/Interpreter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Interpreter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunTest("../../Resources/code.txt");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void RunTest(string filePath)
+        {
+            try
+            {
+                var code = File.ReadAllText(filePath);
+                Interpreter interpreter = new Interpreter();
+                Console.WriteLine($"Running test: {filePath}");
+                interpreter.Interpret(code);
+                Console.WriteLine(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading {filePath}: {ex.Message}");
+            }
+        }
+    }
+}
